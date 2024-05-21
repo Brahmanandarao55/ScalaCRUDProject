a6e2cf9dc1434c72bde4bea5499635096e396dd8
Nandha <brahmanandarao5@gmai.com>
2024-05-21 12:49:19 +0530

    Add Unit Tests for UserCRUD

---
+++ src/test/scala/UserCRUDTest.scala
@@ -0,0 +1,34 @@
+import org.scalatest.flatspec.AnyFlatSpec
+import org.scalatest.matchers.should.Matchers
+
+class UserCRUDTest extends AnyFlatSpec with Matchers {
+  "UserCRUD" should "create, read, update, and delete users" in {
+    val crud = new UserCRUD
+    val user = User(1, "John Doe", "john.doe@example.com")
+
+    // Create
+    crud.createUser(user)
+    crud.readUser(1) should be(Some(user))
+
+    // Update
+    val updatedUser = user.copy(name = "Jane Doe")
+    crud.updateUser(updatedUser)
+    crud.readUser(1) should be(Some(updatedUser))
+
+    // Delete
+    crud.deleteUser(1)
+    crud.readUser(1) should be(None)
+  }
+
+  it should "list all users" in {
+    val crud = new UserCRUD
+    val user1 = User(1, "John Doe", "john.doe@example.com")
+    val user2 = User(2, "Jane Doe", "jane.doe@example.com")
+
+    crud.createUser(user1)
+    crud.createUser(user2)
+
+    val users = crud.listUsers()
+    users should contain theSameElementsAs List(user1, user2)
+  }
+}
